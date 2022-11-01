0a9eb63cac410375551d45a3159aa2cbc7df17b9
chenluo <chenluo.cn@gmail.com>
2022-11-01 23:03:23 +0800

    zk

--- node_exporter.sh
+++ node_exporter.sh
@@ -5,19 +5,19 @@ NODE_EXPORTER_DIR=$WORKSPACE/node_exporter
 if [ $PRE = true ]; then
     URL='https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-amd64.tar.gz'
 
-    NODE_EXPORTER_PACKAGE=node_exporter-1.4.0
+    NODE_EXPORTER_PACKAGE=node_exporter-1.4.0.linux-amd64
     if [ -d $APP_DIR/$NODE_EXPORTER_PACKAGE ];then
         echo 'extracted'
     else
-        if [ -f $APP_DIR/${NODE_EXPORTER_PACKAGE}.linux-amd64.tar.gz ];then
+        if [ -f $APP_DIR/${NODE_EXPORTER_PACKAGE}.tar.gz ];then
             echo 'downloaded'
-            tar zxvf ${NODE_EXPORTER_PACKAGE}.linux-amd64.tar.gz
+            tar zxvf ${NODE_EXPORTER_PACKAGE}.tar.gz
         else
             echo 'download and extracting'
             curl -o $APP_DIR/${NODE_EXPORTER_PACKAGE}.tar.gz -L $URL && tar zxvf ${NODE_EXPORTER_PACKAGE}.tar.gz
         fi
     fi
-    ln -s $APP_DIR/node_exporter-1.4.0.linux-amd64 $WORKSPACE/node_exporter
+    ln -s $APP_DIR/$NODE_EXPORTER_PACKAGE $WORKSPACE/node_exporter
 
     #cp $CONFIG_DIR/grafana.yml $NODE_EXPORTER_DIR/grafana.yml
 fi
---
+++ zookeeper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd $APP_DIR
+ZOOKEEPER_DIR=$WORKSPACE/zookeeper
+if [ $PRE = true ]; then
+    URL='https://dlcdn.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz'
+
+    ZOOKEEPER_PACKAGE=apache-zookeeper-3.8.0-bin
+    if [ -d $APP_DIR/$ZOOKEEPER_PACKAGE ];then
+        echo 'extracted'
+    else
+        if [ -f $APP_DIR/${ZOOKEEPER_PACKAGE}.tar.gz ];then
+            echo 'downloaded'
+            tar zxvf ${ZOOKEEPER_PACKAGE}.tar.gz
+        else
+            echo 'download and extracting'
+            curl -o $APP_DIR/${ZOOKEEPER_PACKAGE}.tar.gz -L $URL && tar zxvf ${ZOOKEEPER_PACKAGE}.tar.gz
+        fi
+    fi
+    ln -s $APP_DIR/$ZOOKEEPER_PACKAGE $WORKSPACE/zookeeper
+
+    cp $CONFIG_DIR/zoo.cfg $ZOOKEEPER_DIR/conf/zoo.cfg
+
+
+fi
+
+if [ $RUN = true ]; then
+    cd $ZOOKEEPER_DIR
+    nohup ./bin/zkServer.sh start 2>&1 > $ZOOKEEPER_DIR/zookeeper.log &
+fi
